a99976f035d3f5d993f746b6195708d072546d6d
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2026-05-21 11:17:31 +0530

    Refactored socketStore to remove socketId management.

--- client/src/store/socketStore.ts
+++ client/src/store/socketStore.ts
@@ -1,16 +1,12 @@
 import { create } from "zustand";
 
 interface SocketState {
-  socketId: string;
-  setSocketId: (socketId: string) => void;
   user: string;
   setUser: (user: string) => void;
 }
 
 const useSocketStore = create<SocketState>((set) => ({
-  socketId: "",
   user: "",
-  setSocketId: (socketId: string) => set({ socketId }),
   setUser: (user: string) => set({ user }),
 }));
 
